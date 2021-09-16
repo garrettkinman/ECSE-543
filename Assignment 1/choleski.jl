4e58ca3fa2479d97973fae3600a6f18252c882b4
Garrett Kinman <garrettkinman@gmail.com>
2021-09-16 09:46:22 -0400

    accounted for floating point error from integer inputs #1 #2

--- Assignment 1/choleski.jl
+++ Assignment 1/choleski.jl
@@ -3,7 +3,7 @@ using Test
 """
 Uses Choleski decomposition to solve `𝐀𝐱 = 𝐛`, where 𝐀 is real, symmetric, and positive-definite. Returns the vector 𝐱.
 """
-function choleski(𝐀::AbstractMatrix{T}, 𝐛::AbstractVector{T})::AbstractVector{T} where {T<:Real}
+function choleski(𝐀::AbstractMatrix{T}, 𝐛::AbstractVector{T}) where {T<:Real}
     # TODO: choleski decomposition
     # for now, use built-in solution to Ax=b
     return 𝐀 \ 𝐛
@@ -11,4 +11,5 @@ end
 
 @testset "real, symmetric, and positive-definite" begin
     @test choleski([1 0; 0 1], [1; 1]) == [1; 1]
+    @test choleski([2 -1 0; -1 2 -1; 0 -1 2], [1; 0; 1]) ≈ [1; 1; 1] # approx is to account for finit-precision floats
 end
