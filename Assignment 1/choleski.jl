43f289cae0d5fc32059922a12afb69259ecd96fe
Garrett Kinman <garrettkinman@gmail.com>
2021-09-16 09:31:02 -0400

    fixed function parametric typing #1 #2

--- Assignment 1/choleski.jl
+++ Assignment 1/choleski.jl
@@ -3,9 +3,12 @@ using Test
 """
 Uses Choleski decomposition to solve `𝐀𝐱 = 𝐛`, where 𝐀 is real, symmetric, and positive-definite. Returns the vector 𝐱.
 """
-function choleski(𝐀::AbstractMatrix{Real}, 𝐛::AbstractVector{Real})::AbstractVector{Real}
+function choleski(𝐀::AbstractMatrix{T}, 𝐛::AbstractVector{T})::AbstractVector{T} where {T<:Real}
     # TODO: choleski decomposition
     # for now, use built-in solution to Ax=b
     return 𝐀 \ 𝐛
 end
 
+@testset "real, symmetric, and positive-definite" begin
+    @test choleski([1 0; 0 1], [1; 1]) == [1; 1]
+end
