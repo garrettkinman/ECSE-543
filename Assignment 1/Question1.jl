81ef24f38fac8c52bb775660d1787008b55582b6
garrettkinman <garrettkinman@gmail.com>
2021-10-14 16:10:19 -0400

    reorganized code; allow convenient use of spread operator

--- Assignment 1/Question1.jl
+++ Assignment 1/Question1.jl
@@ -2,21 +2,5 @@ include("Assignment 1/choleski.jl")
 include("Assignment 1/circuit.jl")
 
 # solve circuit 1 from /circuits/circuit1.toml
-
-function solve_circuit(𝐀, 𝐉, 𝐑, 𝐄)
-    # (𝐀 * 𝐘 * 𝐀ᵀ) * 𝐯 = 𝐀 * (𝐉 - 𝐘 * 𝐄)
-
-    # fill in 𝐘 with zeros everywhere but the main diagonal
-    𝐘 = zeros(length(𝐑), length(𝐑))
-    for i ∈ 1:length(𝐑)
-        𝐘[i,i] = 1.0 / 𝐑[i]
-    end
-
-    # perform choleski decomposition with constructed 𝐀 and 𝐛
-    𝐯 = choleski(𝐀 * 𝐘 * transpose(𝐀), 𝐀 * (𝐉 - (𝐘 * 𝐄)))
-    return 𝐯
-end
-
 circuit = get_circuit(1)
-
-solve_circuit(circuit.𝐀, circuit.𝐉, circuit.𝐑, circuit.𝐄)
+solve_circuit(circuit...)
--- Assignment 1/circuit.jl
+++ Assignment 1/circuit.jl
@@ -11,3 +11,24 @@ function get_circuit(id::Int)
     # need to flatten 𝐀 into a matrix instead of nested vectors
     return ( 𝐀 = transpose(hcat(circuit["A"]...)), 𝐉 = circuit["J"], 𝐑 = circuit["R"], 𝐄 = circuit["E"])
 end
+
+"""
+Solves a circuit given the reduced incidence matrix, 𝐀; current source matrix, 𝐉;
+resistance matrix, 𝐑; and voltage source matrix, 𝐄. Solves using Choleski decomposition.
+
+Tip: use the spread operator, `...`, to use the returned output of `get_circuit(id)`
+without having to access the members of the named tuple.
+"""
+function solve_circuit(𝐀, 𝐉, 𝐑, 𝐄)
+    # (𝐀 * 𝐘 * 𝐀ᵀ) * 𝐯 = 𝐀 * (𝐉 - 𝐘 * 𝐄)
+
+    # fill in 𝐘 with zeros everywhere but the main diagonal
+    𝐘 = zeros(length(𝐑), length(𝐑))
+    for i ∈ 1:length(𝐑)
+        𝐘[i,i] = 1.0 / 𝐑[i]
+    end
+
+    # perform choleski decomposition with constructed 𝐀 and 𝐛
+    𝐯 = choleski(𝐀 * 𝐘 * transpose(𝐀), 𝐀 * (𝐉 - (𝐘 * 𝐄)))
+    return 𝐯
+end
