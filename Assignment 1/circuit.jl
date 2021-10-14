bb739f969f8b13e0e81364644481c69b9d70fe5c
garrettkinman <garrettkinman@gmail.com>
2021-10-14 02:03:16 -0400

    added docstring

--- Assignment 1/circuit.jl
+++ Assignment 1/circuit.jl
@@ -1,5 +1,9 @@
 using TOML
 
+"""
+Retrieves a circuit of a given ID and returns its reduced incidence matrix, 𝐀; current source matrix, 𝐉;
+resistance matrix, 𝐑; and voltage source matrix, 𝐄. These are returned as a named tuple.
+"""
 function get_circuit(id::Int)
     circuit = TOML.tryparsefile("Assignment 1/circuits/circuit$(id).toml")
     return ( 𝐀 = circuit["A"], 𝐉 = circuit["J"], 𝐑 = circuit["R"], 𝐄 = circuit["E"])
