2a2e71731cef1cb773ed811d7b3e77b9a2b052ea
garrettkinman <garrettkinman@gmail.com>
2021-10-14 16:20:53 -0400

    solves circuit 2 #4

--- Assignment 1/Question1.jl
+++ Assignment 1/Question1.jl
@@ -4,3 +4,7 @@ include("Assignment 1/circuit.jl")
 # solve circuit 1 from /circuits/circuit1.toml
 circuit = get_circuit(1)
 solve_circuit(circuit...)
+
+# solve circuit 2 from /circuits/circuit2.toml
+circuit2 = get_circuit(2)
+solve_circuit(circuit2...)
