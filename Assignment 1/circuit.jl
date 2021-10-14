af5883f5b42fcd19f639c96ed3222bf94dbac467
garrettkinman <garrettkinman@gmail.com>
2021-10-14 15:51:30 -0400

    solves simple circuit #4

--- Assignment 1/circuit.jl
+++ Assignment 1/circuit.jl
@@ -6,5 +6,8 @@ resistance matrix, 𝐑; and voltage source matrix, 𝐄. These are returned as 
 """
 function get_circuit(id::Int)
     circuit = TOML.tryparsefile("Assignment 1/circuits/circuit$(id).toml")
-    return ( 𝐀 = circuit["A"], 𝐉 = circuit["J"], 𝐑 = circuit["R"], 𝐄 = circuit["E"])
+
+    # return as named tuple
+    # need to flatten 𝐀 into a matrix instead of nested vectors
+    return ( 𝐀 = transpose(hcat(circuit["A"]...)), 𝐉 = circuit["J"], 𝐑 = circuit["R"], 𝐄 = circuit["E"])
 end
