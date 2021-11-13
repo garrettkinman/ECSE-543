452c8fdada4a1c9ea4f48e73c6511da5a6b18ea3
garrettkinman <garrettkinman@gmail.com>
2021-11-12 23:48:20 -0500

    generates A and b, performs conjugate gradient

---
+++ Assignment 2/choleski.jl
@@ -0,0 +1,96 @@
+using Test
+
+"""
+Uses Choleski decomposition to solve `𝐀𝐱 = 𝐛`, where 𝐀 is real, symmetric, and positive-definite. Returns the vector 𝐱.
+"""
+function choleski(𝐀::AbstractMatrix{<:Real}, 𝐛::AbstractVector{<:Real}, halfband=nothing)
+    n, m = size(𝐀)
+
+    # simple error checking
+    if n != m
+        throw(DimensionMismatch("Matrix 𝐀 must be square"))
+    elseif length(𝐛) != n
+        throw(DimensionMismatch("Matrix 𝐀 must be n×n, and vector 𝐛 must be n×1"))
+    end
+    
+    # initialize
+    𝐋 = zeros(n,n)
+    𝐲 = zeros(n,1)
+    𝐱 = zeros(n,1)
+
+    #decompose 𝐋
+    for j ∈ 1:n
+        sumⱼ = 0
+        for q = 1:j-1
+            @inbounds sumⱼ = sumⱼ + 𝐋[j,q]^2
+        end
+        @inbounds 𝐋[j,j] = sqrt(𝐀[j,j] - sumⱼ)
+
+        sumᵢ = 0
+
+        i_range = (j+1):n
+        if !isnothing(halfband)
+            if (j + halfband + 1) < n
+                i_range = (j+1):(j+halfband)
+            end
+        end
+        for i ∈ i_range
+
+            for k ∈ 1:j-1
+                if k == 1
+                    @inbounds sumᵢ = 𝐋[i,k]*𝐋[j,k]
+                else
+                    @inbounds sumᵢ = sumᵢ + 𝐋[i,k]*𝐋[j,k]
+                end
+            end
+            @inbounds 𝐋[i,j] = (𝐀[i,j] - sumᵢ) / 𝐋[j,j]
+        end
+    end
+
+    #get 𝐲
+    for i ∈ 1:n
+        sumᵧ = 0
+        for j ∈ 1:i-1
+            @inbounds sumᵧ = sumᵧ + 𝐋[i,j]*𝐲[j]
+        end
+        @inbounds 𝐲[i] = (𝐛[i] - sumᵧ) / 𝐋[i,i]
+    end
+    #calculate 𝐱
+    for i ∈ n:-1:1
+        sumₓ = 0
+        for j ∈ (i+1):n
+            @inbounds sumₓ = sumₓ + 𝐋[j,i]*𝐱[j]
+        end
+        @inbounds 𝐱[i] = (𝐲[i] - sumₓ) / 𝐋[i,i]
+    end
+
+    return 𝐱
+end
+
+@testset "real, symmetric, and positive-definite" begin
+    # test a simple 2×2 case
+    test_𝐀 = [1 0; 0 1]
+    test_𝐱 = [1; 1]
+    test_𝐛 = test_𝐀 * test_𝐱
+    @test choleski(test_𝐀, test_𝐛) ≈ test_𝐱 # approx is to account for floating point errors
+
+    # test 10 random 2×2 cases
+    for i ∈ 1:10
+        test_𝐱 = rand(2)
+        test_𝐛 = test_𝐀 * test_𝐱
+        @test choleski(test_𝐀, test_𝐛) ≈ test_𝐱
+    end
+
+    # test a simple 3×3 case
+    test_𝐀 = [2 -1 0; -1 2 -1; 0 -1 2]
+    test_𝐱 = [1; 1; 1]
+    test_𝐛 = [1; 0; 1]
+    @test choleski(test_𝐀, test_𝐛) ≈ test_𝐱
+
+    # test 10 random 3×3 cases
+    for i ∈ 1:10
+        test_𝐱 = rand(3)
+        test_𝐛 = test_𝐀 * test_𝐱
+        @test choleski(test_𝐀, test_𝐛) ≈ test_𝐱
+    end
+end
--- Assignment 2/conjugate-gradient.jl
+++ Assignment 2/conjugate-gradient.jl
@@ -1,70 +1,90 @@
 using Test
 using LinearAlgebra
 include("Assignment 2/mesh.jl")
+include("Assignment 2/choleski.jl")
 
 function generate_matrix(potentials::PotentialMesh, num_nodes::Integer)
     n, m = size(potentials.mesh)
 
-    # initialize
+    # initialize 𝐀 as diagonal matrix with the -4φᵢⱼ set as all the diagonals
+    # need to fill in all the other 1s and 2s below
     𝐀 = one(zeros(num_nodes, num_nodes)) * -4
-    # 𝐀 = identity(n) * -4
+
+    # zero-initialize 𝐛; need to fill in some of the values below
     𝐛 = zeros(num_nodes)
 
     # k is used for indexing into our constructed 𝐀 and 𝐛
     k = 1
 
-    for i ∈ 1:(n-1)
-        for j ∈ 1:(m-1)
-            if j > 2 && potentials.mesh[i, j] == 0 && potentials.mesh[i, j - 1] == potentials.inner_potential
-                if i == 1
+    # manually fill in rest of 𝐀 and 𝐛
+    for i ∈ 1:(n-1) # right boundary are all non-free nodes
+        for j ∈ 1:(m-1) # top boundary are all non-free nodes
+            if j > 2 && potentials.mesh[i, j] == 0 && potentials.mesh[i, j - 1] == potentials.inner_potential # nodes 2, 8
+                if i == 1 # node 2
                     𝐀[k, k + 1] = 1
                     𝐀[k, k + 2] = 2
                     𝐛[k] = -potentials.inner_potential
-                elseif i == 2
-                    𝐀[k, k + 1] = 𝐀[k, k - 2] = 𝐀[k, k + 5] = 1
+                elseif i == 2 # node 8
+                    𝐀[k, k + 1] = 1
+                    𝐀[k, k - 2] = 1
+                    𝐀[k, k + 5] = 1
                     𝐛[k] = -potentials.inner_potential
                 end
                 k += 1
-            elseif j + 1 == m
-                if i == 1
+            elseif j == m - 1 # nodes 3, 9, 15, 21, 27
+                if i == 1 # node 3
                     𝐀[k, k - 1] = 1
                     𝐀[k, k + 2] = 2
                     𝐛[k] = -potentials.outer_potential
-                elseif i == 2
-                    𝐀[k, k - 1] = 𝐀[k, k + 5] = 𝐀[k, k - 2] = 1
-                    𝐛[k] == 0
-                elseif i + 1 == n
-                    𝐀[k, k - 1] = 𝐀[k, k - 5] = 1
+                elseif i == 2 # node 9
+                    𝐀[k, k - 1] = 1
+                    𝐀[k, k + 5] = 1
+                    𝐀[k, k - 2] = 1
+                    𝐛[k] = -potentials.outer_potential
+                elseif i == n - 1 # node 27
+                    𝐀[k, k - 1] = 1
+                    𝐀[k, k - 5] = 1
                     𝐛[k] = -potentials.outer_potential * 2
-                else
-                    𝐀[k, k - 1] = 𝐀[k, k + 5] = 𝐀[k, k - 5] = 1
+                else # nodes 15, 21
+                    𝐀[k, k - 1] = 1
+                    𝐀[k, k + 5] = 1
+                    𝐀[k, k - 5] = 1
+                    𝐛[k] = -potentials.outer_potential
                 end
                 k += 1
-            elseif j == 1 && i > 2
-                if potentials.mesh[i - 1, j] == potentials.inner_potential
+            elseif j == 1 && i > 2 # nodes 11, 17, 23
+                if potentials.mesh[i - 1, j] == potentials.inner_potential # node 11
                     𝐀[k, k + 1] = 2
                     𝐀[k, k + 5] = 1
                     𝐛[k] = -potentials.inner_potential
-                elseif i + 2 == n
+                elseif i == n - 1 # node 23
                     𝐀[k, k + 1] = 2
                     𝐀[k, k - 5] = 1
                     𝐛[k] = -potentials.outer_potential
-                else
+                else # node 17
                     𝐀[k, k + 1] = 2
-                    𝐀[k, k + 5] = 𝐀[k, k - 5] = 1
+                    𝐀[k, k + 5] = 1
+                    𝐀[k, k - 5] = 1
                     𝐛[k] = 0
                 end
                 k += 1
-            elseif i == 3 && potentials.mesh[i - 1, j] == potentials.inner_potential
-                𝐀[k, k - 1] = 𝐀[k, k + 1] = 𝐀[k, k + 5] = 1
+            elseif i == 3 && potentials.mesh[i - 1, j] == potentials.inner_potential # nodes 12, 13
+                𝐀[k, k - 1] = 1
+                𝐀[k, k + 1] = 1
+                𝐀[k, k + 5] = 1
                 𝐛[k] = -potentials.inner_potential
                 k += 1
-            elseif i + 1 == n
-                𝐀[k, k - 1] = 𝐀[k, k + 1] = 𝐀[k, k - 5] = 1
+            elseif i == n - 1 # nodes 24, 25, 26
+                𝐀[k, k - 1] = 1
+                𝐀[k, k + 1] = 1
+                𝐀[k, k - 5] = 1
                 𝐛[k] = -potentials.outer_potential
                 k += 1
-            elseif i > 2 && j > 1
-                𝐀[k, k - 1] = 𝐀[k, k + 1] = 𝐀[k, k - 5] = 𝐀[k, k + 5] = 1
+            elseif i > 2 && j > 1 # nodes 14, 18, 19, 20
+                𝐀[k, k - 1] = 1
+                𝐀[k, k + 1] = 1
+                𝐀[k, k - 5] = 1
+                𝐀[k, k + 5] = 1
                 𝐛[k] = 0
                 k += 1
             end
@@ -76,7 +96,7 @@ end
 ## debug
 
 potentials = PotentialMesh(h=0.02)
-generate_matrix(potentials, 19)
+mesh_𝐀, mesh_𝐛 = generate_matrix(potentials, 19)
 
 ## conjugate gradient
 
@@ -149,6 +169,8 @@ end
 
 # @test conjugate_gradient(test_𝐀, test_𝐛) ≈ test_𝐱
 
+conjugate_gradient(transpose(mesh_𝐀) * mesh_𝐀, transpose(mesh_𝐀) * mesh_𝐛)
+choleski(transpose(mesh_𝐀) * mesh_𝐀, transpose(mesh_𝐀) * mesh_𝐛)
 ## test
 
 @testset "real, symmetric, and positive-definite" begin
